66b224c3cc6416ef7bb98c61588aaf7a03f1a1a1
Frederick Malcolm <frederickmalcolm@Fredericks-MacBook-Pro.local>
2025-12-28 22:49:39 +0700

    Update Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,24 +7,38 @@ const Projects = () => {
   const projectList = [
     {
       category: "Software Application",
-      title: "ExploreID",
-      desc: "A high-frequency trading bot designed to detect and execute arbitrage opportunities between stocks and derivatives using Python and WebSocket API.",
-      tags: ["Python", "Asyncio", "Pandas", "WebSockets"],
+      title: "ExploreID - University Project",
+      desc: "A tourism software application that allows users to search for destinations and tour guides, while providing personalized recommendations using an AI-based recommender system.",
+      tags: ["Python", "React", "Pandas", "SupaBase", "JavaScript"],
       link: "https://exploreid-one.vercel.app" 
     },
     {
-      category: "Computer Vision",
-      title: "Real-time Traffic Analysis System",
-      desc: "Developed a YOLOv8-based model to classify and count vehicle types on toll roads, assisting in congestion monitoring and data collection.",
-      tags: ["PyTorch", "YOLOv8", "OpenCV", "Deep Learning"],
-      link: "#"
+      category: "Personal Portfolio",
+      title: "Personal Portfolio Website",
+      desc: "Designed and developed a responsive personal portfolio website to showcase projects, technical skills, and experience.",
+      tags: ["JavaScript", "CSS", "HTML", "Vercel", "Json"],
+      link: "https://frederickportfolio-nine.vercel.app/"
     },
     {
-      category: "Financial Research",
-      title: "Sentiment Analysis on Market News",
-      desc: "Utilizing NLP transformers (BERT) to scrape and analyze 10,000+ financial news articles for predicting market bullish/bearish trends.",
-      tags: ["NLP", "Transformers", "Scikit-learn", "Data Science"],
-      link: "#"
+      category: "AI/Deep Learning",
+      title: "ASL Recognition App",
+      desc: "Developed a deep learning-based system to recognize American Sign Language (ASL) hand gestures in real time using computer vision techniques.",
+      tags: ["MediaPipe", "OpenCV", "Data Science", "Python", "Sklearn"],
+      link: "https://github.com/Fred0105/ASL-Hand-Pattern-Recognition"
+    },
+    {
+      category: "Natural Language Processing",
+      title: "Sentiment Analysis",
+      desc: "Analyzed sentiment from Ethereum-related discussions on X (Twitter) and examined its correlation with price volatility using natural language processing techniques.",
+      tags: ["Python", "Vader", "NLTK", "Matplotlib"],
+      link: "https://colab.research.google.com/drive/1lrJ2Hv9uAPXytBbzm-GpR_1g0-7mogCY?usp=sharing"
+    },
+    {
+      category: "Data Science / Machine Learning (Academic)",
+      title: "Dividend-Based Stock Price Analysis Using Machine Learning",
+      desc: "An academic machine learning project analyzing stock price behavior around dividend periods in Indonesia’s banking and energy sectors using Random Forest and XGBoost.",
+      tags: ["Python", "RandomForest", "Data Analysis", "Machine Learning"],
+      link: "/rm.pdf"
     }
   ];
 
